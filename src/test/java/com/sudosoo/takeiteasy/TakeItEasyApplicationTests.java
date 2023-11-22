fd1ae8d1e14e7b7c542d7ad7ed613442b81b9b8c
antidote <>
2023-11-22 19:23:56 +0900

    jenkins 테스트어노테이션 실패 수정

--- src/test/java/com/sudosoo/takeiteasy/TakeItEasyApplicationTests.java
+++ src/test/java/com/sudosoo/takeiteasy/TakeItEasyApplicationTests.java
@@ -2,8 +2,7 @@ package com.sudosoo.takeiteasy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active:prod")
 class TakeItEasyApplicationTests {
 
     @Test
