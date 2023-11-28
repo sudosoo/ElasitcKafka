b3adc79554e852b54e783d6e550e738a4a1f0dfc
antidote <>
2023-11-28 15:04:31 +0900

    feat/webHook test

--- src/main/java/com/sudosoo/takeiteasy/config/KafkaConfig.java
+++ src/main/java/com/sudosoo/takeiteasy/config/KafkaConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
