ca9eeeda00831d4964dd6f336fd527abac099d84
antidote <>
2023-12-13 23:51:42 +0900

    feat/loggingTest

--- src/main/java/com/sudosoo/takeiteasy/controller/ProfileController.java
+++ src/main/java/com/sudosoo/takeiteasy/controller/ProfileController.java
@@ -1,6 +1,7 @@
 package com.sudosoo.takeiteasy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ProfileController {
 
-    @GetMapping(value = "/check" , name = "API Check")
+    @PostMapping(value = "/check" , name = "API Check")
     public String checkServerStatus(){
         return "check";
     }
 
-    @GetMapping(value = "/exception" , name = "API Exception test")
+    @PostMapping(value = "/exception" , name = "API Exception test")
     public IllegalArgumentException exceptionServerStatus() {
         return new IllegalArgumentException("Exception test");
     }
