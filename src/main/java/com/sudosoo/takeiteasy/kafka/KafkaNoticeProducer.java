9a21ff99f02ee58fabf39c3f7a7afda5816b1348
username <email@email.com>
2024-02-15 17:04:48 +0900

    feat/카프카 비동기 통신 만들기

--- src/main/java/com/sudosoo/takeiteasy/kafka/KafkaNoticeProducer.java
+++ src/main/java/com/sudosoo/takeiteasy/kafka/KafkaNoticeProducer.java
@@ -1,6 +1,11 @@
 package com.sudosoo.takeiteasy.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sudosoo.takeiteasy.dto.KafkaRequestMember;
+import com.sudosoo.takeiteasy.dto.RequestMessage;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -15,6 +20,23 @@ public class KafkaNoticeProducer {
 
     public void sendNotice(String memberName, String requestMessage) {
         kafkaTemplate.send(kafkaNoticeTopic, memberName ,requestMessage);
+    }
+
+    public void send(Object requestMessage) {
+        kafkaTemplate.send(kafkaNoticeTopic, requestMessage);
+    }
+
+
+    public void sendCreatePostRequest(KafkaRequestMember data) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String requestData = objectMapper.writeValueAsString(new RequestMessage("createPost", data));
+            ProducerRecord<String, String> record = new ProducerRecord<>(kafkaNoticeTopic, requestData);
+
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendTest(String message) {
