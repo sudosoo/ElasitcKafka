572f7596830b7672fa913588b3c570cc641999ee
username <email@email.com>
2024-02-21 15:48:56 +0900

    feat/멤버서버분리

--- src/main/java/com/sudosoo/takeiteasy/entity/Member.java
+++ src/main/java/com/sudosoo/takeiteasy/entity/Member.java
@@ -1,38 +1,38 @@
-package com.sudosoo.takeiteasy.entity;
-
-import com.sudosoo.takeiteasy.common.BaseEntity;
-import com.sudosoo.takeiteasy.dto.member.CreateMemberRequestDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@EqualsAndHashCode(callSuper=false)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member  extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String memberName;
-
-    private boolean isDeleted = false;
-
-    private Member(CreateMemberRequestDto createMemberRequestDto) {
-        this.memberName = createMemberRequestDto.getMemberName();
-    }
-    public static Member of(CreateMemberRequestDto createMemberRequestDto){
-        return new Member(createMemberRequestDto);
-    }
-
-    public void disableMember() {
-        this.isDeleted = true;
-    }
-
-}
+//package com.sudosoo.takeiteasy.entity;
+//
+//import com.sudosoo.takeiteasy.common.BaseEntity;
+//import com.sudosoo.takeiteasy.dto.member.CreateMemberRequestDto;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.AccessLevel;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Getter
+//@EqualsAndHashCode(callSuper=false)
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//public class Member  extends BaseEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String memberName;
+//
+//    private boolean isDeleted = false;
+//
+//    private Member(CreateMemberRequestDto createMemberRequestDto) {
+//        this.memberName = createMemberRequestDto.getMemberName();
+//    }
+//    public static Member of(CreateMemberRequestDto createMemberRequestDto){
+//        return new Member(createMemberRequestDto);
+//    }
+//
+//    public void disableMember() {
+//        this.isDeleted = true;
+//    }
+//
+//}
