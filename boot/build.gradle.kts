8befbc03346b1a21adfdac0c46450f79fcd242d5
antidote <ljh8651@nate.com>
2024-03-25 18:57:46 +0900

    fix/build 테스트

--- boot/build.gradle.kts
+++ boot/build.gradle.kts
@@ -9,6 +9,6 @@ dependencies {
     implementation(project(":takeItEasy-presentation"))
     implementation(project(":takeItEasy-aop"))
 
-    runtimeOnly("org.postgresql:postgresql")
+//    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.batch:spring-batch-test")
 }
