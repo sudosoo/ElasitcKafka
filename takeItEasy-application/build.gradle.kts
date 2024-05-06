5f3ba0215bfd27a2c5b92121f627abce8dda3e8b
antidote <ljh8651@nate.com>
2024-05-06 16:16:09 +0900

    feat/elastic 실행 완료

--- takeItEasy-application/build.gradle.kts
+++ takeItEasy-application/build.gradle.kts
@@ -2,8 +2,10 @@ dependencies {
     api(project(":takeItEasy-domain"))
 
     implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter")
+
 
     testImplementation("org.springframework.batch:spring-batch-test")
 
